bfdaaff8e874217d802165c47d5c830efb8d0573
Jakub <148567047+Xmausix@users.noreply.github.com>
2026-04-06 17:54:52 +0200

    add new deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,15 @@
-#!/usr/bin/env sh
-set -eu
-
-npm ci
+# 1. Rebuild
+rm -rf .vitepress/dist
+rm -rf .vitepress/cache
 npm run build
+# 2. Update build in ../mterczynski.github.io
+rm -rf ../mterczynski.github.io/blog
+mkdir ../mterczynski.github.io/blog
+cp -r ./.vitepress/dist/* ../mterczynski.github.io/blog/
+cd ../mterczynski.github.io/blog
 
-# Required:
-# DEPLOY_BRANCH=gh-pages
-# Optional:
-# DEPLOY_REMOTE=origin
+#3. Commit in ../mterczynski.github.io
+git commit -a -m "Update blog: $(git log -1 --pretty=format:"%s")"
 
-: "${DEPLOY_BRANCH:?DEPLOY_BRANCH is required}"
-DEPLOY_REMOTE="${DEPLOY_REMOTE:-origin}"
-
-BUILD_DIR=".vitepress/dist"
-TMP_DIR=".deploy-tmp"
-
-# Clean temp dir
-rm -rf "$TMP_DIR"
-mkdir "$TMP_DIR"
-
-# Copy build output
-cp -r "$BUILD_DIR/." "$TMP_DIR/"
-
-cd "$TMP_DIR"
-
-# Init git repo
-git init
-git checkout -b "$DEPLOY_BRANCH"
-
-# Add remote repo (assumes current repo origin)
-REPO_URL=$(git -C .. config --get remote.origin.url)
-git remote add origin "$REPO_URL"
-
-# Commit & push
-git add .
-git commit -m "deploy: update site"
-
-git push -f "$DEPLOY_REMOTE" "$DEPLOY_BRANCH"
-
-cd ..
-rm -rf "$TMP_DIR"
+# 4. Push in ../mterczynski.github.io
+git push
