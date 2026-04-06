cbcfd0b37076911ed82a7f33824caa995a5827d1
Michał Terczyński <mterczynski1@gmail.com>
2026-04-06 20:09:22 +0200

    Blog: Roll out to PROD!

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ rm -rf .vitepress/dist
 rm -rf .vitepress/cache
 npm run build
 # 2. Update build in ../mterczynski.github.io
-rm -rf ../mterczynski.github.io/blog-vitepress
-mkdir ../mterczynski.github.io/blog-vitepress
-cp -r ./.vitepress/dist/* ../mterczynski.github.io/blog-vitepress/
+rm -rf ../mterczynski.github.io/vitepress
+mkdir ../mterczynski.github.io/vitepress
+cp -r ./.vitepress/dist/* ../mterczynski.github.io/vitepress/
 cd ../mterczynski.github.io
 
 #3. Commit in ../mterczynski.github.io
-git add blog-vitepress;
+git add vitepress;
 git commit -m "Update blog: $(git log -1 --pretty=format:"%s")"
 
 # 4. Push in ../mterczynski.github.io
