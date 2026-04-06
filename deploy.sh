42bd19aee0ec6312d131b4629aaa0736c1b6faa7
Michał Terczyński <mterczynski1@gmail.com>
2026-04-06 19:49:16 +0200

    Update deploy script & basepath

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,14 @@ rm -rf .vitepress/dist
 rm -rf .vitepress/cache
 npm run build
 # 2. Update build in ../mterczynski.github.io
-rm -rf ../mterczynski.github.io/blog
-mkdir ../mterczynski.github.io/blog
-cp -r ./.vitepress/dist/* ../mterczynski.github.io/blog/
-cd ../mterczynski.github.io/blog
+rm -rf ../mterczynski.github.io/blog-vitepress
+mkdir ../mterczynski.github.io/blog-vitepress
+cp -r ./.vitepress/dist/* ../mterczynski.github.io/blog-vitepress/
+cd ../mterczynski.github.io
 
 #3. Commit in ../mterczynski.github.io
-git commit -a -m "Update blog: $(git log -1 --pretty=format:"%s")"
+git add blog-vitepress;
+git commit -m "Update blog: $(git log -1 --pretty=format:"%s")"
 
 # 4. Push in ../mterczynski.github.io
 git push
