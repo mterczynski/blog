9bc5884cee818968b1046f6e72352d020fc5c021
Michał Terczyński <mterczynski1@gmail.com>
2026-04-06 22:17:06 +0200

    Oops

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@ rm -rf .vitepress/dist
 rm -rf .vitepress/cache
 npm run build
 # 2. Update build in ../mterczynski.github.io
-rm -rf ../mterczynski.github.io/vitepress
-mkdir ../mterczynski.github.io/vitepress
-cp -r ./.vitepress/dist/* ../mterczynski.github.io/vitepress/
+rm -rf ../mterczynski.github.io/blog
+mkdir ../mterczynski.github.io/blog
+cp -r ./.vitepress/dist/* ../mterczynski.github.io/blog/
 cd ../mterczynski.github.io
 
 #3. Commit in ../mterczynski.github.io
-git add vitepress;
+git add blog;
 git commit -m "Update blog: $(git log -1 --pretty=format:"%s")"
 
 # 4. Push in ../mterczynski.github.io
